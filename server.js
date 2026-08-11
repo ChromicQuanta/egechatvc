9a6134981602a62a52ea9dfa6ae5cba16607b836
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2026-08-11 23:09:39 +1000

    e

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ ws.acc = 0
         if(!ws.acc){
             ws.acc=1
             acc++
-buf = add(new Float32Array(new Uint8Array(e.data.buffer).buffer),buf)
+buf = add(new Float32Array(new Uint8Array(e.data).buffer),buf)
 
 EE=new Uint8Array(e.data).length//(new Uint8Array(e.data.buffer)).length//Float32Array(buf)
 
